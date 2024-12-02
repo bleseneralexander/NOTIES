c0ab92b022d3fb9451eb996d3f25099f95feef42
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-12-02 09:28:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ const pusher = new Pusher({
 pusher.trigger("my-channel", "my-event", {
   message: "hello world"
 });
+
+
+var events = new List<Event>();
+// count can be anything between 1 and 10000
+for (int i = 1; i <= count; i++)
+{
+    // creating a sample list of events
+    events.Add(new Event
+    {
+        Channel = string.Format("batch-channel-{0}", i),
+        EventName = "batch-event",
+        Data = new
+        {
+            Channel = string.Format("batch-channel-{0}", i),
+            Event = "batch-event",
+            Message = string.Format("{0} - sample message", i)
+        {
+    });
+}
+var result = new List<HttpStatusCode>();
+int chunkSize = 10;
+int totalChunks = (int)Math.Ceiling((double)events.Length / chunkSize);
+for (int i = 0; i < totalChunks; i++)
+{
+    var eventsChunk = events.Skip(i * chunkSize).Take(chunkSize).ToArray();
+    // publishing event lists of size 10
+    var chunkResult = await pusher.TriggerAsync(eventsChunk);
+    result.Add(chunkResult.StatusCode);
+}
